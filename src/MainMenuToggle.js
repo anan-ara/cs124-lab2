0d137bbcb87a8a0f53e032cb9edbd54e6ccf8f17
Cynthia Hom <chom@g.hmc.edu>
2022-03-24 01:24:17 -0700

    Fix deploy script error

--- src/MainMenuToggle.js
+++ src/MainMenuToggle.js
@@ -11,7 +11,7 @@ function MainMenuToggle(props) {
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
         </svg>
-        <label>Menu</label>
+        {/* <label>Menu</label> */}
       </button>
     );
   }
