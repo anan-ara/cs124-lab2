006e7924a67998860186994ad487b7cb2a5af482
Anan A <anan.ara2001@gmail.com>
2022-03-24 11:56:15 -0700

    finish radio button functionality

---
+++ src/CompletionBar.js
@@ -0,0 +1,21 @@
+import "./MainMenu.css";
+import "./Dropdown.css";
+import "./CompletionBar.css";
+// import {useState} from "react";
+
+function CompletionBar(props) {
+
+  return (
+      <div className="completion-bar">
+    <button className={(props.showCompleted ? "activated " : "") + "radio-button" } onClick={props.onShowCompleted}> 
+    Show All Tasks
+    </button>
+    <button className={(!props.showCompleted ? "activated " : "") + "radio-button" } onClick={props.onHideCompleted}> 
+    Show Only Incomplete Tasks
+    </button>
+
+      </div>
+  );
+}
+
+export default CompletionBar;
