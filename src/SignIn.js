ff567fc6edf66bae8c9b8d313feb257deb615c9a
Anan A <anan.ara2001@gmail.com>
2022-05-03 17:08:24 -0700

    finish adding error messages

--- src/SignIn.js
+++ src/SignIn.js
@@ -7,9 +7,8 @@ import {
   useSignInWithEmailAndPassword,
   useSignInWithGoogle,
 } from "react-firebase-hooks/auth";
-import GoogleButton from 'react-google-button'
-import GithubButton from 'react-github-login-button'
-
+import GoogleButton from "react-google-button";
+import GithubButton from "react-github-login-button";
 
 import { useState } from "react";
 
@@ -22,58 +21,58 @@ function SignIn(props) {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const errorMessageMap = {
- "Firebase: Error (auth/invalid-email).":  "Email address does not appear to be valid. Please try again.",
- "Firebase: Error (auth/wrong-password).":  "Incorrect password.",
- "Firebase: Error (auth/user-not-found).":  "Email does not exist. Please sign up for an account."
-  }
+    "Firebase: Error (auth/invalid-email).":
+      "Email address does not appear to be valid. Please try again.",
+    "Firebase: Error (auth/wrong-password).": "Incorrect password.",
+    "Firebase: Error (auth/user-not-found).":
+      "Email does not exist. Please sign up for an account.",
+  };
 
-  if (user1 || user2) {
-    // Shouldn't happen because App should see that
-    // we are signed in.
-    return <div>Unexpectedly signed in already</div>;
-  } else if (loading1 || loading2) {
-    return <div>Logging In...</div>;
-  }
   return (
-    <>
-      <div></div>
-      <div className="sign-in-popup popup">
-        <div className="menu-title">Login</div>
-        <div className="login-fields">
-            {error1 && <div className="error-message">{errorMessageMap[error1.message]}</div>}
-        {error1 && console.log(error1.message)}
-          <label htmlFor="email">Email: </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div className="sign-in-popup popup">
+      {user1 || user2 || loading1 || loading2 ? (
+        ((user1 || user2) && <div>Unexpectedly signed in already</div>) ||
+        ((loading1 || loading2) && <div>Logging In...</div>)
+      ) : (
+        <>
+          <div className="menu-title">Login</div>
+          {error1 && (
+            <div className="error-message">
+              {errorMessageMap[error1.message]}
+            </div>
+          )}
+          {error1 && console.log(error1.message)}
+          <form className="login-fields">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="pw">Password: </label>
+            <input
+              type="password"
+              id="pw"
+              value={pw}
+              autoComplete="current-password"
+              onChange={(e) => setPw(e.target.value)}
+            />
+          </form>
           <br />
-          <label htmlFor="pw">Password: </label>
-          <input
-            type="password"
-            id="pw"
-            value={pw}
-            onChange={(e) => setPw(e.target.value)}
-          />
-        </div>
-        <br />
-        <div className="login-buttons">
-          <button onClick={() => signInWithEmailAndPassword(email, pw)}>
-            Sign in
-          </button>
-          <button onClick={props.onToggleSignUp}>
-            Sign up
-          </button>
-        </div>
-        <hr />
-        <GoogleButton className="google"
-          onClick={() => signInWithGoogle()} />
-        <GithubButton 
-          onClick={() => signInWithGoogle()} />
-      </div>
-    </>
+          <div className="login-buttons">
+            <button onClick={() => signInWithEmailAndPassword(email, pw)}>
+              Sign in
+            </button>
+            <button onClick={props.onToggleSignUp}>Sign up</button>
+          </div>
+          <hr />
+          <GoogleButton className="google" onClick={() => signInWithGoogle()} />
+          <GithubButton onClick={() => signInWithGoogle()} />
+        </>
+      )}
+    </div>
   );
 }
 
--- src/SignUp.js
+++ src/SignUp.js
@@ -10,84 +10,62 @@ import { initializeApp } from "firebase/app";
 import { useState } from "react";
 
 function SignUp(props) {
-  const [createUserWithEmailAndPassword, userCredential, loading, error] =
+  const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(props.auth);
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
 
   const errorMessageMap = {
- "Firebase: Error (auth/invalid-email).":  "Email address does not appear to be valid. Please try again.",
- "Firebase: Error (auth/internal-error).":  "Password cannot be empty."
-  }
+    "Firebase: Error (auth/invalid-email).":
+      "Email address does not appear to be valid. Please try again.",
+    "Firebase: Error (auth/internal-error).": "Password cannot be empty.",
+  };
 
-    // function createUser(res) {
-    //   if (!error) {
-    //       console.log("res.user is " + res.user)
-    //       // console.log("user is " + user);
-    //       console.log("userCredential is " + userCredential);
-    //       console.log("loading is  " + loading);
-    //       console.log("error is " +  error);
-    //     setDoc(doc(usersCollection, userCredential.uid), {
-    //       uid: userCredential.uid,
-    //       highPriorityIcon: "",
-    //       lowPriorityIcon: "",
-    //       medPriorityIcon: "",
-    //       sort: "created",
-    //       email: userCredential.email,
-    //     });
-    //   }
-    // }
-
-  if (userCredential) {
-    // Shouldn't happen because App should see that
-    // we are signed in.
-    return <div>Unexpectedly signed in already</div>;
-  } else if (loading) {
-    return <p>Signing up…</p>;
-  }
   return (
-    <>
-      <div></div>
-      <div className="sign-in-popup popup">
-        <div className="menu-title">Sign Up</div>
-        <div className="login-fields">
-          {error && <div className="error-message">{errorMessageMap[error.message]}</div>}
-          {error && console.log(error.message)}
-          <label htmlFor="email">Email: </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div className="sign-in-popup popup">
+      {user || loading ? (
+        (user && <div>Unexpectedly signed in already</div>) ||
+        (loading && <div>Signing up...</div>)
+      ) : (
+        <>
+          <div className="menu-title">Sign Up</div>
+          <div className="login-fields">
+            {error && (
+              <div className="error-message">
+                {errorMessageMap[error.message]}
+              </div>
+            )}
+            {error && console.log(error.message)}
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="pw">Password: </label>
+            <input
+              type="password"
+              id="pw"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+            />
+          </div>
           <br />
-          <label htmlFor="pw">Password: </label>
-          <input
-            type="password"
-            id="pw"
-            value={pw}
-            onChange={(e) => setPw(e.target.value)}
-          />
-        </div>
-        <br />
-        <div className="login-buttons sign-up-buttons">
-          <button
-            onClick={
-              props.onToggleSignUp
-            }
-          >
-          Go back
-          </button>
-          <button
-            onClick={
-              () => createUserWithEmailAndPassword(email, pw) //.then(props.onCreateUser)
-            }
-          >
-          Sign up
-          </button>
-        </div>
-      </div>
-    </>
+          <div className="login-buttons sign-up-buttons">
+            <button onClick={props.onToggleSignUp}>Go back</button>
+            <button
+              onClick={
+                () => createUserWithEmailAndPassword(email, pw) //.then(props.onCreateUser)
+              }
+            >
+              Sign up
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
