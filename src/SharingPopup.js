3609299d723110e4aa88580976edf5e250152bd1
Anan A <anan.ara2001@gmail.com>
2022-05-06 18:12:26 -0700

    add validation

--- src/SharingPopup.js
+++ src/SharingPopup.js
@@ -24,9 +24,18 @@ function SharingPopup(props) {
     console.log("handling change");
     setValue(value);
   }
+  
+  // Regular expression from https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  function validateEmail(email) {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
 
   function addValue(newValue) {
-    setValue([...value, { label: newValue, value: newValue }]);
+    if (validateEmail(newValue)) {
+      setValue([...value, { label: newValue, value: newValue }]);
+    }
     setInputValue("");
   }
 
