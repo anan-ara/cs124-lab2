e6b07a9dfa15466cc11033498d90e45d04dbd5fc
Cynthia Hom <chom@g.hmc.edu>
2022-05-06 22:43:20 -0700

    fixed regex

--- src/SharingPopup.js
+++ src/SharingPopup.js
@@ -23,9 +23,10 @@ function SharingPopup(props) {
 
   // Regular expression from https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
   function validateEmail(email) {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+    // return email.match(
+    //   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    // );
+    return email.match(/\S+@\S+\.\S+/);
   }
 
   function addValue(newValue) {
